a9214f6b926515d3776c232c91e356891c129230
xsdxq <1299873646@qq.com>
2022-04-29 18:09:32 +0800

    图像识别优化

--- utils/ImageIdentify.py
+++ utils/ImageIdentify.py
@@ -16,12 +16,21 @@ from paddleocr import PaddleOCR, draw_ocr
 from utils.response_code import RET, error_map_EN
 
 
+def GetMiddleStr(content, startStr, endStr):
+    startIndex = content.index(startStr)
+    if startIndex >= 0:
+        startIndex += len(startStr)
+    endIndex = content.index(endStr)
+    return content[startIndex:endIndex]
+
+
 def ImageIdentify(img_path):
     # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
     ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
 
     # 正则匹配
-    pattern_time = "检测时间.{,2}(\d{4}-\d{2}-\d{2})"
+    pattern_time1 = "检测时间.{,2}(\d{4}-\d{2}-\d{2})"
+    pattern_time2 = ".{,2}(\d{4}-\d{2}-\d{2})核酸检测时间"
     pattern_name = "姓名[^\u4e00-\u9fa5]*([\u4e00-\u9fa5]*\W*)身份证"
     pattern_result = ".*(阳性|阴性).*"
     try:
@@ -37,14 +46,23 @@ def ImageIdentify(img_path):
         if len(re_name) > 0:
             name = re_name[0]
         else:
-            name = None
+            re_name = GetMiddleStr(retxt, '姓名', '身份证')
+            if len(re_name) > 0:
+                name = re_name
+            else:
+                name = ""
 
-        re_time = re.findall(pattern_time, retxt)
+        re_time = re.findall(pattern_time1, retxt)
         print("re_time", re_time)
         if len(re_time) > 0:
             time = re_time[0]
+        # 更改匹配模式
         else:
-            time = None
+            re_time = re.findall(pattern_time2, retxt)
+            if len(re_time) > 0:
+                time = re_time[0]
+            else:
+                time = ""
 
         re_check_result = re.findall(pattern_result, retxt)
         print("check_result", re_check_result)
@@ -54,21 +72,21 @@ def ImageIdentify(img_path):
                 check_result = '无法识别'
 
         else:
-            check_result = None
+            check_result = "无法识别"
 
-        if name and time and check_result != None:
+        # if name and time and check_result != None:
 
-            # 识别结果
-            re_data = {
-                "name": name,
-                "time": time,
-                "result": check_result,
-            }
+        # 识别结果
+        re_data = {
+            "name": name,
+            "time": time,
+            "result": check_result,
+        }
 
-            return {'code': RET.OK, 'message': error_map_EN[RET.OK], 'data': re_data}
+        return {'code': RET.OK, 'message': error_map_EN[RET.OK], 'data': re_data}
 
-        else:
-            return {'code': RET.IMAGEERR, 'message': "图像识别失败，可能上传了非核酸检测结果截图", 'data': "图像识别失败，可能上传了非核酸检测结果截图"}
+        # else:
+        #     return {'code': RET.IMAGEERR, 'message': "图像识别失败，可能上传了非核酸检测结果截图", 'data': "图像识别失败，可能上传了非核酸检测结果截图"}
 
     except Exception as e:
         return {'code': RET.IMAGEERR, 'message': "图像识别失败，请上传北京健康宝/支付宝健康码截图", 'data': "图像识别失败，请上传北京健康宝/支付宝健康码截图"}
